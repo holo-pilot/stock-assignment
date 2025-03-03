42d99e1aeb26aecb839456e11fccd3e6b0aac352
holo-pilot <hans.opena@outlook.com>
2025-03-03 22:32:28 +0000

    took ; out  to check if values would show in database

--- stockpy.py
+++ stockpy.py
@@ -21,13 +21,13 @@ cursor.execute(command1)
 
 # Adding values
 
-cursor.execute("INSERT INTO inventory VALUES ( 1, 'tea', 20, 2025-05-10);")
+cursor.execute("INSERT INTO inventory VALUES ( 1, 'tea', 20, 2025-05-10)")
 
-cursor.execute("INSERT INTO inventory VALUES ( 2, 'coffee', 40, 2025-05-10);")
+cursor.execute("INSERT INTO inventory VALUES ( 2, 'coffee', 40, 2025-05-10)")
 
-cursor.execute("INSERT INTO inventory VALUES ( 3, 'coke', 35, 2025-05-10);")
+cursor.execute("INSERT INTO inventory VALUES ( 3, 'coke', 35, 2025-05-10)")
 
-cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, 2025-05-10);")
+cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, 2025-05-10)")
 
 # Data check
 
