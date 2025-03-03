4521947eea77a682c49ec67eeae905b5ae19bc25
holo-pilot <hans.opena@outlook.com>
2025-03-03 22:29:07 +0000

    Added some example values in database

--- stockpy.py
+++ stockpy.py
@@ -10,9 +10,28 @@ cursor = connection.cursor()
 
 command1 = """CREATE TABLE IF NOT EXISTS inventory(
    stock_id INT PRIMARY KEY NOT NULL,
-   product TEXT,
-   amount INT,
-   expiry TEXT
+   product TEXT NOT NULL,
+   amount INT NOT NULL,
+   expiry TEXT NOT NULL
 ); """
 
+
 cursor.execute(command1)
+
+
+# Adding values
+
+cursor.execute("INSERT INTO inventory VALUES ( 1, 'tea', 20, 2025-05-10);")
+
+cursor.execute("INSERT INTO inventory VALUES ( 2, 'coffee', 40, 2025-05-10);")
+
+cursor.execute("INSERT INTO inventory VALUES ( 3, 'coke', 35, 2025-05-10);")
+
+cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, 2025-05-10);")
+
+# Data check
+
+# cursor.execute("SELECT * FROM inventory;")
+# results = cursor.fetchall()
+# print(results)
+
