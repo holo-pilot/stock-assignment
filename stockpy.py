df2d085d6ff29bba6723eef21f46dfb03372047f
holo-pilot <hans.opena@outlook.com>
2025-03-04 19:24:58 +0000

    Fixed issue of values not showing in database viewer and added line to close connection

--- stockpy.py
+++ stockpy.py
@@ -8,12 +8,12 @@ cursor = connection.cursor()
 
 # Create table
 
-command1 = """CREATE TABLE IF NOT EXISTS inventory(
+command1 = '''CREATE TABLE IF NOT EXISTS inventory(
    stock_id INT PRIMARY KEY NOT NULL,
    product TEXT NOT NULL,
    amount INT NOT NULL,
    expiry TEXT NOT NULL
-); """
+); '''
 
 
 cursor.execute(command1)
@@ -21,13 +21,18 @@ cursor.execute(command1)
 
 # Adding values
 
-cursor.execute("INSERT INTO inventory VALUES ( 1, 'tea', 20, 2025-05-10)")
+cursor.execute("INSERT INTO inventory VALUES ( 1, 'tea', 20, '2025-05-05')")
 
-cursor.execute("INSERT INTO inventory VALUES ( 2, 'coffee', 40, 2025-05-10)")
+cursor.execute("INSERT INTO inventory VALUES ( 2, 'coffee', 40, '2025-06-06')")
 
-cursor.execute("INSERT INTO inventory VALUES ( 3, 'coke', 35, 2025-05-10)")
+cursor.execute("INSERT INTO inventory VALUES ( 3, 'coke', 35, '2025-06-07')")
 
-cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, 2025-05-10)")
+cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, '2025-06-06')")
+
+
+# Commit the changes to the database to for it to show in the viewer
+
+connection.commit() 
 
 # Data check
 
@@ -35,3 +40,6 @@ cursor.execute("INSERT INTO inventory VALUES ( 4,'pepsi', 69, 2025-05-10)")
 # results = cursor.fetchall()
 # print(results)
 
+# Close the connection between python and SQLite
+
+connection.close()
